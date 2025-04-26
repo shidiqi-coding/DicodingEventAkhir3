3fcb73e4d762a57073993217ed5382b72a8176b3
shidiqi-coding <shidiqiihsan@gmail.com>
2025-04-26 21:12:06 +0700

    Update MainActivity.kt

--- app/src/main/java/com/dicoding/dicodingevent/MainActivity.kt
+++ app/src/main/java/com/dicoding/dicodingevent/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dicoding.dicodingevent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import androidx.viewpager2.widget.ViewPager2
@@ -22,21 +21,21 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.MainToolbar)
 
-        val viewPager: ViewPager2= binding.viewPager
+        val viewPager: ViewPager2 = binding.viewPager
         val navView: BottomNavigationView = binding.navView
 
-        //pasang ViewPage2 adapter
-        val adapter= ViewPagerAdapter(this)
+       
+        val adapter = ViewPagerAdapter(this)
         viewPager.adapter = adapter
 
-        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                navView.menu.getItem(position).isChecked= true
+                navView.menu.getItem(position).isChecked = true
             }
         })
 
-        navView.setOnNavigationItemSelectedListener {item ->
+        navView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> viewPager.currentItem = 0
                 R.id.navigation_upcoming -> viewPager.currentItem = 1
@@ -47,10 +46,11 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        val destination = intent.getStringExtra("destination")
+        if (destination == "upcoming") {
+            viewPager.currentItem = 1
+        }
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-
-//        supportActionBar?.hide()
     }
 }
