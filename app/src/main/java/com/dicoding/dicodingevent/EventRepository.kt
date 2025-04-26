14580471b854b26423fd2e0be7c789cc62c21660
shidiqi-coding <shidiqiihsan@gmail.com>
2025-04-26 21:10:01 +0700

    Update EventRepository.kt

--- app/src/main/java/com/dicoding/dicodingevent/EventRepository.kt
+++ app/src/main/java/com/dicoding/dicodingevent/EventRepository.kt
@@ -1,7 +1,6 @@
 package com.dicoding.dicodingevent
 
 import androidx.lifecycle.LiveData
-import com.dicoding.dicodingevent.data.response.DetailEventResponse
 import com.dicoding.dicodingevent.data.response.EventResponse
 import com.dicoding.dicodingevent.data.retrofit.ApiService
 import com.dicoding.dicodingevent.entity.FavoriteEvent
@@ -9,26 +8,33 @@ import com.dicoding.dicodingevent.entity.FavoriteEventDao
 import retrofit2.Call
 
 
-class EventRepository(private val apiService: ApiService,  private val favoriteEventDao: FavoriteEventDao) {
-    fun getEventsCallback(active: Int , query: String?): Call<EventResponse> {
-        return apiService.getEvent(active , query)
+class EventRepository(
+    private val apiService: ApiService ,
+    private val favoriteEventDao: FavoriteEventDao
+) {
+    fun getFinishedEventsCallback(query: String?): Call<EventResponse> {
+        return apiService.getEvent(active = 0 , q = query)
+    }
+
+    fun getUpcomingEventsCallback(query: String?): Call<EventResponse> {
+        return apiService.getEvent(active = 1 , q = query)
     }
 
     fun getAllFavorites(): LiveData<List<FavoriteEvent>> {
         return favoriteEventDao.getAllFavorites()
     }
 
-//    fun getEventDetail(id: String): Call<DetailEventResponse> {
-//        return apiService.getDetailEvents(id)
-//    }
 
     companion object {
         @Volatile
         private var instance: EventRepository? = null
 
-        fun getInstance(apiService: ApiService, favoriteEventDao: FavoriteEventDao): EventRepository {
+        fun getInstance(
+            apiService: ApiService ,
+            favoriteEventDao: FavoriteEventDao
+        ): EventRepository {
             return instance ?: synchronized(this) {
-                instance ?: EventRepository(apiService, favoriteEventDao).also { instance = it }
+                instance ?: EventRepository(apiService , favoriteEventDao).also { instance = it }
             }
         }
     }
