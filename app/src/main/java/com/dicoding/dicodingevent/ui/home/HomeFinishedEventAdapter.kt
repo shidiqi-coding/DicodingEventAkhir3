c57edd1e641bec261cd83f5d9333e8bf0018b4c2
shidiqi-coding <shidiqiihsan@gmail.com>
2025-04-26 20:55:46 +0700

    Update HomeFinishedEventAdapter.kt

--- app/src/main/java/com/dicoding/dicodingevent/ui/home/HomeFinishedEventAdapter.kt
+++ app/src/main/java/com/dicoding/dicodingevent/ui/home/HomeFinishedEventAdapter.kt
@@ -11,42 +11,51 @@ import com.dicoding.dicodingevent.databinding.ItemVerticalFinishedBinding
 import com.dicoding.dicodingevent.data.response.ListEventsItem
 
 class HomeFinishedEventAdapter(private val onItemClick: (ListEventsItem) -> Unit) :
-   ListAdapter<ListEventsItem, HomeFinishedEventAdapter.EventViewHolder>(EventDiffCallBack()) {
+    ListAdapter<ListEventsItem , HomeFinishedEventAdapter.EventViewHolder>(EventDiffCallBack()) {
 
-       override fun onCreateViewHolder(parent: ViewGroup, viwType : Int): EventViewHolder {
-           val binding = ItemVerticalFinishedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-           return EventViewHolder(binding ,onItemClick)
+    override fun onCreateViewHolder(parent: ViewGroup , viwType: Int): EventViewHolder {
+        val binding = ItemVerticalFinishedBinding.inflate(
+            LayoutInflater.from(parent.context) ,
+            parent ,
+            false
+        )
+        return EventViewHolder(binding , onItemClick)
 
-       }
+    }
 
-    override fun onBindViewHolder(holder: EventViewHolder, position : Int) {
+    override fun onBindViewHolder(holder: EventViewHolder , position: Int) {
         val event = getItem(position)
         holder.bind(event)
 
     }
 
-    class EventViewHolder(private val binding : ItemVerticalFinishedBinding, private val onItemClick: (ListEventsItem) -> Unit) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(event : ListEventsItem) {
+    class EventViewHolder(
+        private val binding: ItemVerticalFinishedBinding ,
+        private val onItemClick: (ListEventsItem) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(event: ListEventsItem) {
             with(binding) {
                 tvItemFinished.text = event.name
-                //tvFinishedDescription.text = event.description
                 Glide.with(itemView.context)
                     .load(event.imageLogo)
                     .into(binding.imgFinishedPhoto)
 
             }
-            itemView.setOnClickListener{
+            itemView.setOnClickListener {
                 onItemClick(event)
             }
-         }
+        }
     }
 
     private class EventDiffCallBack : DiffUtil.ItemCallback<ListEventsItem>() {
-        override fun areItemsTheSame(oldItem: ListEventsItem, newItem: ListEventsItem): Boolean {
+        override fun areItemsTheSame(oldItem: ListEventsItem , newItem: ListEventsItem): Boolean {
             return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: ListEventsItem, newItem: ListEventsItem): Boolean {
+        override fun areContentsTheSame(
+            oldItem: ListEventsItem ,
+            newItem: ListEventsItem
+        ): Boolean {
             return oldItem == newItem
 
         }
